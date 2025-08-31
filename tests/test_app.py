ac511719eff34e1d0c7e6184d160f0d2fd56d8f4
david <david.marivat-larose@orange.fr>
2025-08-31 18:12:25 +0200

    Update test_app.py.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
-# tests/test_app.py
+import sys
+import os
+
+# Ajouter le dossier courant à sys.path pour que Python trouve api.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from api import app
 from fastapi.testclient import TestClient
-from api import app  # ou ton fichier principal
 
 client = TestClient(app)
 
